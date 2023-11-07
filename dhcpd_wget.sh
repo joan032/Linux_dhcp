cc36ea9225a4aec0f5002776dfbf35b534024376
joan032 <102249490+joan032@users.noreply.github.com>
2023-11-07 15:48:16 +0100

    dhcpd_wget.sh

--- dhcpd_wget.sh
+++ dhcpd_wget.sh
@@ -1,23 +1,24 @@
 #!/bin/bash
-# Purpose: Config ubuntu DHCP server
+# Purpose: Configure Ubuntu DHCP server
 # https://ubuntu.com/server/docs/network-dhcp
 # Author: Jose Maria Madronal GPL v2.0+ (Proxmox script)
 # ------------------------------------------
-#
-#
+
 # declare STRING variable
-STRING="Scripts examples"
-f_dchpd_conf="https://github.com/joan032/Linux_dhcp.git/dhcpd.conf"
-#print variable on a screen
+STRING="Script example"
+f_dchpd_conf="https://raw.githubusercontent.com/joan032/Linux_dhcp/master/dhcpd.conf"
+
+# Print variable on the screen
 echo $STRING
 
-#Install DHCP
-apt-get install isc-dhcp-server
+# Install DHCP
+sudo apt-get install isc-dhcp-server
 
-# download githup configuration file
+# Download GitHub configuration file
 wget $f_dchpd_conf
 
-# copy configuration file to etc directory
-cp dhcpd.conf /etc/dhcp
+# Copy the configuration file to /etc/dhcp directory
+sudo cp dhcpd.conf /etc/dhcp/
 
-systemctl restart isc-dhcp-server
+# Restart the DHCP server
+sudo systemctl restart isc-dhcp-server
